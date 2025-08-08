7e9bccb216125b9e216cf31f2dbc00559440ec7f
hankakukatakana <65936854+hankakukatakana@users.noreply.github.com>
2025-08-08 11:59:24 +0900

    up

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@ set APP_NAME=RentalServerChecker
 echo ===================================
 echo  %APP_NAME% - Compiling Start
 echo ===================================
-pyinstaller --name "%APP_NAME%" --onefile --windowed --add-data "web;web" main.py
+pyinstaller --name "%APP_NAME%" --onefile --windowed --add-data "web;web" --icon=img/RentalServerChecker.ico main.py
 echo ===================================
 echo  Compiling Completion
 echo ===================================
